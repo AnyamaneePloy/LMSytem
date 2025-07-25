4c314140055e0e5701ac8de5105fdee005158fa0
AnyamaneePloy <anyamanee@techgateway.co.th>
2025-07-25 16:53:46 +0700

    update api data

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
-const API_BASE_URL = 'https://lmsytem-production.up.railway.app/'|| 'http://localhost:4000';; // from Railway
+const API_BASE_URL =  'http://localhost:4000';
 
 
 export async function getClosedCases() {
