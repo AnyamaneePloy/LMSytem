2722b20d39def1fd82e942d4afd58fc3a33938e8
AnyamaneePloy <anyamanee@techgateway.co.th>
2025-07-25 17:28:31 +0700

    update admi.js

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,5 +1,7 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
-// const API_BASE_URL =  'http://localhost:4000';
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://lmsytem-production.up.railway.app'
+    : 'http://localhost:4000';
 
 
 export async function getClosedCases() {
